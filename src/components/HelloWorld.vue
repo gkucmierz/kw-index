24f7d78720cc83a42728517ec8e61f9c5ffc219a
Grzegorz Kućmierz <gkucmierz@gmail.com>
2025-09-16 12:45:51 +0200

    add multiselect

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,26 @@
 <script setup>
 import { ref } from 'vue';
+import MultiSelect from 'primevue/multiselect';
+
+const cities = [
+  { name: 'aaaa' },
+  { name: 'aaab' },
+  { name: 'aaac' },
+  { name: 'aaad' },
+  { name: 'aaae' },
+];
+
+let selectedCities = ref([]);
 </script>
 
 <template>
-  lorem
+  <!-- lorem -->
+  <!-- {{ cities }} -->
+
+  {{ selectedCities }}
+
+  <MultiSelect v-model="selectedCities" :options="cities" optionLabel="name" filter placeholder="Select Cities"
+    :selectionLimit="1" class="w-full md:w-80" />
 </template>
 
 <style scoped>
